96fa4b1815980f18e3b2bc05688eb5ee67493848
Florian Lopes <florian.lopes@outlook.com>
2016-10-27 07:36:02 +0200

    Enables application to take PID 1 and receive SIGTERM signals. SIGTERM signals are sent by Docker stop command. See here https://docs.docker.com/engine/reference/builder/#/entrypoint

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-if [ $DEBUG ]; then
+
+if [ "$DEBUG" = true ]; then
   ADDITIONAL_PARAMETERS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n"
 fi
 
-java $ADDITIONAL_PARAMETERS \
+# Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
+# See here https://docs.docker.com/engine/reference/builder/#/entrypoint
+exec java $ADDITIONAL_PARAMETERS \
        -Djava.security.egd=file:/dev/./urandom -jar ${APP_HOME}/${APP_JAR}
